4dae4fd124c9541e190294fc9777aaf8c8898612
mo_yu_jian <73953348+sugarymother@users.noreply.github.com>
2020-12-05 19:37:24 +0800

    first

---
+++ src/main/java/com/linghang/skimlessons/SkimlessonsApplication.java
@@ -0,0 +1,13 @@
+package com.linghang.skimlessons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkimlessonsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkimlessonsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/linghang/skimlessons/SkimlessonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linghang.skimlessons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkimlessonsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
